ad286389c697410044532fda1c8a8de6a4000d61
abdullah-umar-ali <abdallahomar2108@gmail.com>
2022-07-20 00:48:26 +0200

    fix automation deployment

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,11 @@ const products = require("./routes/porducts")
 require('dotenv').config()
 const port = process.env.PORT || 4000 
 
+
+app.use('/api/product' , products )
+
 app.listen(port , () => {
     console.log(`server is running on port ${port}`)
 })
 
-app.use('/api/product' , products )
 
